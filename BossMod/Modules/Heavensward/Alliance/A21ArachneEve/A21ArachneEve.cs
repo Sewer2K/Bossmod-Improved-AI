bd5c0fc375f7e1400bc00e4b2f9dd6d3085752dc
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-04-18 04:18:15 -0500

    few corrections after live run

--- BossMod/Modules/Heavensward/Alliance/A21ArachneEve/A21ArachneEve.cs
+++ BossMod/Modules/Heavensward/Alliance/A21ArachneEve/A21ArachneEve.cs
@@ -1,14 +1,14 @@
 ﻿namespace BossMod.Heavensward.Alliance.A21ArachneEve;
 
 [ModuleInfo(BossModuleInfo.Maturity.Contributed, Contributors = "LTS", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 168, NameID = 4871)]
-public class A21ArachneEve(WorldState ws, Actor primary) : BossModule(ws, primary, new ArenaBoundsCircle(new(16, -60), 35))
+public class A21ArachneEve(WorldState ws, Actor primary) : BossModule(ws, primary, new ArenaBoundsCircle(new(20, -60), 30))
 {
     protected override void DrawEnemies(int pcSlot, Actor pc)
     {
         Arena.Actors(Enemies(OID.Boss), ArenaColor.Enemy);
         Arena.Actors(Enemies(OID.Keyknot), ArenaColor.Enemy);
         Arena.Actors(Enemies(OID.Webmaiden), ArenaColor.Enemy);
-        Arena.Actors(Enemies(OID.SpittingSpider), ArenaColor.Enemy)
+        Arena.Actors(Enemies(OID.SpittingSpider), ArenaColor.Enemy);
         Arena.Actors(Enemies(OID.SkitteringSpider), ArenaColor.Enemy);
         Arena.Actors(Enemies(OID.EarthAether), ArenaColor.Enemy);
         Arena.Actors(Enemies(OID.DeepEarthAether), ArenaColor.Enemy);
--- BossMod/Modules/Heavensward/Alliance/A22Forgall/A22Forgall.cs
+++ BossMod/Modules/Heavensward/Alliance/A22Forgall/A22Forgall.cs
@@ -1,7 +1,7 @@
 ﻿namespace BossMod.Heavensward.Alliance.A22Forgall;
 
 [ModuleInfo(BossModuleInfo.Maturity.Contributed, Contributors = "LTS", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 168, NameID = 4878)]
-public class A22Forgall(WorldState ws, Actor primary) : BossModule(ws, primary, new ArenaBoundsCircle(new(-300, -422), 30))
+public class A22Forgall(WorldState ws, Actor primary) : BossModule(ws, primary, new ArenaBoundsCircle(new(-300, -416), 30))
 {
     protected override void DrawEnemies(int pcSlot, Actor pc)
     {
